bb12701f9b7027274f22fe683c6e922b56ef64d0
JBattishill <jbattishill91@gmail.com>
2022-10-12 16:32:16 +1100

    perfected image search & Started weatherData

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-// Variable to hold Keys
-var geoKey = "234979e2ff9e423095e4b2c869c1c97b";
-var altImageKey = "AIzaSyBxx6AQZClDeZ0easaXPDvnjd0-Gc6RDd8"
-var imageKey = "AIzaSyDpsLn14cZzKJi0o0lQhMmwplnjl5wLN74"
-var imageSearchID = '474f1fff433d047cf'
+// Wikipedia Variables
 
-// Variables for future data
+// Weather Variables
+var weatherKey = "dcc0a4770fed667dbb2a12e8f1a86009";
+var travelTime = ""
+var destinationLat = "48.85"
+var destinationLon = "2.34"
+
+// Timestamp Variables - North and South Hemisphere
+const stampNorthWinter = "1514768400";
+const stampNorthSpring = "1522548000";
+const stampNorthSummer = "1530410400";
+const stampNorthAutumn = "1538359200";
+
+const stampSouthSummer = "1514768400";
+const stampSouthAutumn = "1522548000";
+const stampSouthWinter = "1530410400";
+const stampSouthSpring = "1538359200";
+
+// Geolocation Variables
+var geoKey = "234979e2ff9e423095e4b2c869c1c97b";
 var hemisphere = "";
-var travelSeason = "";
+
+//GoogleAPI Variables
+var altImageKey = "AIzaSyBxx6AQZClDeZ0easaXPDvnjd0-Gc6RDd8";
+var testImageKey ="AIzaSyBTVa6aO4sDlu7EPqm5jvvejyD4nQWC8yw";
+var imageKey = "AIzaSyDpsLn14cZzKJi0o0lQhMmwplnjl5wLN74";
+var imageSearchID = '474f1fff433d047cf';
 var searchQuery = "";
+var travelSeason = "";
 
 function getPhotos(){    
     $(document).ready(function () {        
-        var url = 'https://www.googleapis.com/customsearch/v1?searchType=image&num=9&key=' + altImageKey + '&cx=' + imageSearchID + '&q=' + searchQuery;
+        var url = 'https://www.googleapis.com/customsearch/v1?imgSize=LARGE&imgType=photo&siteSearchFilter=i&imgColorType=color&searchType=image&num=9&key=' + testImageKey + '&cx=' + imageSearchID + '&q=' + searchQuery;
         console.log(url)
         $.getJSON(url, function (apiData) {
 
@@ -19,14 +39,14 @@ function getPhotos(){
             var item = apiData.items[i];
                     var imgSrc = item.link;
                     var imgAlt = item.title;
+                    var imgTitle = item.snippet;
             // Make sure HTML in item.htmlTitle is escaped.
-            var imgtag = $('<img class=""src='+ imgSrc + ' alt=' + imgAlt + '>')
+            var imgtag = $('<img class=""src='+ imgSrc + ' alt=' + imgAlt + ' title=' + imgTitle + '>')
             $('.resultPhotos').append(imgtag)
-           
         }
     });
     var userLocation = document.getElementById('travelLocation').value;
-    var photoCity = $('<h1 class= "capMonth">').html(userLocation);
+    var photoCity = $('<h1 class= "capMonth">').html(userLocation + ' in ' + travelSeason);
     $('.photoHeading').append(photoCity);
 });
 
@@ -81,7 +101,90 @@ function getPhotos(){
         //     };
         //   })
 }
-                                                                                    
+
+function getWeather(){
+    // Calls functions to get timestamps for weatherData
+    if (hemisphere == "North") {
+        getStampNorth();
+        console.log(travelTime)
+        }
+        else  {
+        getStampSouth();
+        console.log(travelTime)
+        }
+    
+    $(document).ready(function () {
+    var userLocation = document.getElementById('travelLocation').value;
+    var url = 'https://history.openweathermap.org/data/3.0/history/timemachine?lat=' + destinationLat + '&lon=' + destinationLon + '&dt=' + travelTime + '&appid=' + weatherKey
+
+    $.getJSON(url, function (weatherData) {
+    
+    var item = weatherData.results[0];
+    console.log(item)
+    //variables to make it easier to target relevant data from the API
+    // var storeCity = item.city;
+    // var storeCountry = item.country;
+    // var storeLatt = item.lat
+    // var storeLong = item.lon
+    // var userMonth = travelMonth;
+    
+
+
+//  // Get Content on the page
+    
+//     // Importing month from user input
+//     var userMonth = document.getElementById('travelMonth').value;
+
+//     // Creating display variables for the different items
+//     var city = $('<h1>').html('City: ' + storeCity);
+//     var country = $('<h2>').html('Country: ' + storeCountry);
+//     var long = $('<h4>').html('Longitude: ' + storeLong);
+//     var latt = $('<h4>').html('Lattitude: ' + storeLatt);
+//     var hemi = $('<h4>').html('Hemisphere: ' + hemisphere);
+//     var month = $('<h2 class="capitilise">').html('Month: ' + userMonth);  
+//     var season = $('<h2>').html('Season: ' + travelSeason);
+    
+//      // Appending the display variables to relevant container ID
+//     $('.containerWeather').append(city,country,long,latt,hemi,month,season);
+    
+    });
+    });
+}
+function getStampNorth(){
+
+    if (travelSeason = "Winter"){
+        travelTime = stampNorthWinter}
+
+    else if (travelSeason = "Spring"){
+        travelTime = stampNorthSpring}
+       
+    else if (travelSeason = "Summer"){
+            travelTime = stampNorthSummer}
+            
+    else if (travelSeason = "Autumn"){
+            travelTime = stampNorthAutumn}
+
+    else {console.log("There was an issue with function getStampNorth")}
+}
+
+function getStampSouth(){
+
+    if (travelSeason = "Winter"){
+        travelTime = stampSouthWinter}
+
+    else if (travelSeason = "Spring"){
+        travelTime = stampSouthSpring}
+       
+    else if (travelSeason = "Summer"){
+            travelTime = stampSouthSummer}
+            
+    else if (travelSeason = "Autumn"){
+            travelTime = stampSouthAutumn}
+
+    else {console.log("There was an issue with function getStampSouth")}
+}
+
+
 function getData(){    
     $(document).ready(function () {
         var userLocation = document.getElementById('travelLocation').value;
@@ -130,13 +233,17 @@ function getData(){
         // Removing submit button and replacing with reset button
         document.getElementById("submitBtn").classList.add('hidden');
         document.getElementById("resetBtn").classList.remove('hidden');
+        document.getElementById("idLocation").classList.remove('hidden');
 
         // Set search query
-        searchQuery = ("Tourist" + userLocation + travelSeason);
+        searchQuery = (userLocation + " in " + travelSeason);
         console.log(searchQuery);
 
-        // calling function to get Photos from google image search
-        getPhotos()
+        // calling function to get weather from OpenWeather API
+        getWeather()
+
+        // calling function to get Photos from google image search API, filtered to images from www.unsplash.com
+        // getPhotos()
 
         });
     });
