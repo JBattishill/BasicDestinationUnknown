ca0545f0a3f5da1cd4742d875542f7c653c5ac20
JBattishill <jbattishill91@gmail.com>
2022-10-12 17:29:55 +1100

    connected to weatherAPI, not targeted results in date or location but its a start.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Wikipedia Variables
 
 // Weather Variables
-var weatherKey = "dcc0a4770fed667dbb2a12e8f1a86009";
+var weatherKey = "dcc0a4770fed667dbb2a12e8f1a86009"
 var travelTime = ""
 var destinationLat = "48.85"
 var destinationLon = "2.34"
 
-// Timestamp Variables - North and South Hemisphere
+// Timestamp Variables - North and South Hemispheres
 const stampNorthWinter = "1514768400";
 const stampNorthSpring = "1522548000";
 const stampNorthSummer = "1530410400";
@@ -104,84 +104,94 @@ function getPhotos(){
 
 function getWeather(){
     // Calls functions to get timestamps for weatherData
-    if (hemisphere == "North") {
-        getStampNorth();
-        console.log(travelTime)
-        }
+    if (hemisphere === "North") {
+        getStampNorth();}
+
         else  {
         getStampSouth();
-        console.log(travelTime)
         }
-    
+
     $(document).ready(function () {
     var userLocation = document.getElementById('travelLocation').value;
-    var url = 'https://history.openweathermap.org/data/3.0/history/timemachine?lat=' + destinationLat + '&lon=' + destinationLon + '&dt=' + travelTime + '&appid=' + weatherKey
+    // var url = 'http://history.openweathermap.org/data/3.0/history/locations/create?lat=' + destinationLat + '&lon=' + destinationLon + '&dt=' + travelTime + '&appid=' + weatherKey
 
+
+    var url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + weatherKey;     
     $.getJSON(url, function (weatherData) {
     
-    var item = weatherData.results[0];
+        console.log(weatherData)
+
+    var item = weatherData.list[0];
     console.log(item)
-    //variables to make it easier to target relevant data from the API
-    // var storeCity = item.city;
-    // var storeCountry = item.country;
-    // var storeLatt = item.lat
-    // var storeLong = item.lon
-    // var userMonth = travelMonth;
+
+    // cannot get targetted location or historical data atm but here is some random data from Moscow
+    // This was really a logic check to make sure I could do things, I can format things and change temps etc later. 
     
+    
+    // variables target relevant data from the API
+    var storeTemp = item.main.temp;
+    var storeFeels = item.main.feels_like;
+    var storeHumidity = item.main.humidity;
+    var storeWind = item.wind.speed;
+    var storeWeather = item.weather[0].main;
+    var storeCloud = item.weather[0].description;
+    var storeCity = weatherData.city.name;
 
 
-//  // Get Content on the page
+    // Get Content on the page
     
-//     // Importing month from user input
-//     var userMonth = document.getElementById('travelMonth').value;
-
-//     // Creating display variables for the different items
-//     var city = $('<h1>').html('City: ' + storeCity);
-//     var country = $('<h2>').html('Country: ' + storeCountry);
-//     var long = $('<h4>').html('Longitude: ' + storeLong);
-//     var latt = $('<h4>').html('Lattitude: ' + storeLatt);
-//     var hemi = $('<h4>').html('Hemisphere: ' + hemisphere);
-//     var month = $('<h2 class="capitilise">').html('Month: ' + userMonth);  
-//     var season = $('<h2>').html('Season: ' + travelSeason);
+    // Creating display variables for the different items
+    var temp = $('<h3>').html('Temp: ' + storeTemp);
+    var feels = $('<h3>').html('Temp Feels like: ' + storeFeels);
+    var humidity = $('<h3>').html('Humidity: ' + storeHumidity);
+    var wind = $('<h3>').html('Wind: ' + storeWind);
+    var weatherMain = $('<h3>').html('Weather Main: ' + storeWeather);
+    var description = $('<h3 class="capitilise">').html('Description: ' + storeCloud);  
+    var icon = $('<h2>').html('City: '+ storeCity);
     
-//      // Appending the display variables to relevant container ID
-//     $('.containerWeather').append(city,country,long,latt,hemi,month,season);
+     // Appending the display variables to relevant container ID
+    $('.containerWeather').append(icon,temp,feels,humidity,wind,weatherMain,description);
     
-    });
-    });
+    }); 
+   });
 }
-function getStampNorth(){
 
-    if (travelSeason = "Winter"){
+function getStampNorth(){
+    console.log(travelTime)
+
+    if (travelSeason === "Winter"){
         travelTime = stampNorthWinter}
 
-    else if (travelSeason = "Spring"){
+    else if (travelSeason === "Spring"){
         travelTime = stampNorthSpring}
        
-    else if (travelSeason = "Summer"){
+    else if (travelSeason === "Summer"){
             travelTime = stampNorthSummer}
             
-    else if (travelSeason = "Autumn"){
+    else if (travelSeason === "Autumn"){
             travelTime = stampNorthAutumn}
 
     else {console.log("There was an issue with function getStampNorth")}
+    console.log(travelTime)
 }
 
 function getStampSouth(){
-
-    if (travelSeason = "Winter"){
+    console.log(travelTime)
+    if (travelSeason === "Winter"){
         travelTime = stampSouthWinter}
 
-    else if (travelSeason = "Spring"){
+
+    else if (travelSeason === "Spring"){
         travelTime = stampSouthSpring}
        
-    else if (travelSeason = "Summer"){
+    else if (travelSeason === "Summer"){
             travelTime = stampSouthSummer}
             
-    else if (travelSeason = "Autumn"){
+    else if (travelSeason === "Autumn"){
             travelTime = stampSouthAutumn}
 
     else {console.log("There was an issue with function getStampSouth")}
+    console.log(travelTime)
 }
 
 
